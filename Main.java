3c2e8a10b625d47b73654c756528f7c9fb1bfe3c
NatassjaDAvila <101428806+natassjad@users.noreply.github.com>
2022-03-12 16:40:11 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    String name = "Natassja dos Reis D'Avila";
+    String nationality = "Brazilian";
+    String biggestDream = "";
+    System.out.println("Name: " + name + "\nNationality: " + nationality + "\nMy Biggest Dream: " + biggestDream);
+}
+}
