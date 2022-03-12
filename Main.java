bf132bdcc963d4ccd0ed29e611ed4c11912caea7
Natassja D'Avila <101428806+natassjad@users.noreply.github.com>
2022-03-12 17:03:02 +0000

    code

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String[] args) {
     String name = "Natassja dos Reis D'Avila";
     String nationality = "Brazilian";
-    String biggestDream = "";
+    String biggestDream = "travel around the world to learn new cultures and languages";
     System.out.println("Name: " + name + "\nNationality: " + nationality + "\nMy Biggest Dream: " + biggestDream);
 }
 }
